d953efc333c1a5c68d5768d854487db7d4c8c5be
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2026-03-05 17:36:28 -0300

    fix: display artist name when uploading files

--- run.py
+++ run.py
@@ -289,6 +289,10 @@ def get_media_info():
     metadata_command = {"command": ["get_property", "filtered-metadata"]}
     metadata_response = send_mpv_command(ipc_path, metadata_command)
 
+    # Command to get filtered metadata
+    filtered_metadata_command = {"command": ["get_property", "filtered-metadata"]}
+    filtered_metadata_response = send_mpv_command(ipc_path, filtered_metadata_command)
+
     # Check if media is playing (core-idle is false when playing)
     if (json.loads(playing_response)['data'] == False):
         is_playing = json.loads(playing_response)['data'] == False
@@ -296,6 +300,10 @@ def get_media_info():
         metadata = json.loads(metadata_response)['data']['Uploader'] if 'Uploader' in json.loads(metadata_response)['data'] else ''
         if ' - Topic' in metadata:
             metadata = metadata.replace(' - Topic', '').strip()
+
+        artist = json.loads(filtered_metadata_response)['data']['Artist'] if 'Artist' in json.loads(filtered_metadata_response)['data'] else ''
+        if metadata == '' and artist != '':
+            metadata = artist
 
         return jsonify({
             'is_playing': is_playing,
