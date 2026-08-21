880512b1724629418e4e411c5d5d07ca2a3cbee4
Carlos Quiroz <carlos.quiroz.90@gmail.com>
2026-08-21 14:26:10 -0300

    refactor: remove unused code from clean_tmp_folder

--- run.py
+++ run.py
@@ -627,8 +627,6 @@ def clean_tmp_folder():
 
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
 
     except Exception as e:
         print('Failed to delete tmp folder: ' + str(e))
